f45bdbb267b8c0670765051701cd5c49766b8971
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-26 10:52:01 +0300

    fixes view function

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 
 # Create your views here.
 def welcome(request):
+  """
+  View function that renders the index page
+  """
   return HttpResponse('Welcome to Gallery')
