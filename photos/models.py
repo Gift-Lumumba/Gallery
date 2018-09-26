5011e208fc3340262f5fb8dd68ae332c5cf2efcf
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-26 15:23:16 +0300

    created Image class and its properties

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model): 
+  name = models.CharField(max_length = 30)
+  image = models.ImageField(upload_to = 'images/')
+  description = models.TextField()
+  location = models.ForeignKey(Location)
+  category = models.ForeignKey(Category)
+
+  def __str__(self):
+      return self.name
+
