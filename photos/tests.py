bc674132235d09f53e58a8c6752873d4f0cfd5e1
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-26 15:57:16 +0300

    creates LocationTestClass which is the test class for the Location module

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Location,Category,Image
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+  
+    # Set up method
+    def setUp(self):
+        self.nairobi= Location(name = 'Nairobi', description ='This is the capital city of Kenya')
