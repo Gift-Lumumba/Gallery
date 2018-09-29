4da1deb0888b389dc1b96d0e455623ec9cd61902
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-29 22:42:12 +0300

    fixes js

--- static/js/copy.js
+++
@@ -1,9 +0,0 @@
-function copyLink() {
-  var copyText = document.getElementById("copyimagelink");
-
-  copyText.select();
-
-  document.execCommand("copy");
-
-  alert("Image link has been successfully copied: " + copyText.value);
-}
