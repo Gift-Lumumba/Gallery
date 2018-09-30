004ea22b0c83e6678da2a62fd3b60dda1579e9d4
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-30 16:30:04 +0300

    made fixes and redeployment

--- static/js/copy_link.js
+++ static/js/copy_link.js
@@ -1,9 +1,12 @@
-function copyLink() {
-  var copyText = document.getElementById("copyimagelink");
-
-  copyText.select();
-
-  document.execCommand("copy");
-
-  alert("Image link has been successfully copied: " + copyText.value);
-}
+// function copyLink(value) {
+//   console.log('copied link')
+//   function handler(event){
+//     event.clipboardData.setData('text/plain', value);
+//     event.preventDefault();
+//     document.removeEventListener('copy', handler, true);
+//   }
+//   document.addEventListener('copy', handler, true);
+//   document.execCommand('copy');
+  
+//   alert("Image link has been successfully copied: " + copyLink.value);
+// }
